24024ffa53ab80cc8945ecfed94b8096d381b8f8
Olivier Van hamme <nicefiction@gmail.com>
2021-08-21 15:15:57 +0200

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Olivier Van hamme on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
